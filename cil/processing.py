44621dc54bf0de97dc59762f0822c92e4f8ce027
gomigom <nodam89@naver.com>
2020-12-22 13:00:18 +0900

    Complete example5

--- cil/processing.py
+++ cil/processing.py
@@ -10,14 +10,29 @@ def invert(img):
 
 # 이미지 합성
 def merge(img1, img2):
-    # img1과 img2의 크기가 같은지 확인
-    if  ### 코드를 작성해 주세요 ### :
-
-        ### 코드를 작성해 주세요 ###
-
+    new_img = []
+    if get_size(img1) == get_size(img2):
+        for h in range(len(img1)):
+            new_img.append([int(or_bits(img1[h][w], img2[h][w])) for w in range(len(img1[0]))])
         return new_img
     else:
         print('img1과 img2의 크기가 같아야 합니다!')
+
+
+# # 모범답안
+# def merge(img1, img2):
+#     # img1과 img2의 크기가 같은지 확인
+#     if get_size(img1) == get_size(img2):
+#
+#         [height, width] = get_size(img1)
+#         new_img = empty_image(height, width)
+#         for i in range(height):
+#             for j in range(width):
+#                 new_img[i][j] = or_bits(img1[i][j], img2[i][j])
+#
+#         return new_img
+#     else:
+#         print('img1과 img2의 크기가 같아야 합니다!')
 
 
 # 이미지 크기를 계산해 주는 함수
